45fd15d9c5eebc7ec95fc85747a692ba0e5beeed
echevarriae <echevarriaeliud@gmail.com>
2017-06-13 20:34:12 -0400

    food feed

---
+++ Food Feed/script/main.js
@@ -0,0 +1,8 @@
+$('.element').instagramElement({
+   mode : 'multiuser',
+   userID : '618517613,1821250268',
+   accessToken : '421631593.e029fea.0ab43b7d616641c9929ee8ec2112d8ed',
+   galleryMode: 'classic',
+   video: true,
+   showVideoControls: false
+});
